ebbe0fa1c7136c89cf351078cb1e024c3a17282b
Six-S <brenlink@gmail.com>
2022-02-07 21:03:57 -0700

    committing here so that I can pull on my laptop. :)

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-from govee_control import Govee
+import sys
+import govee_control
 
 if __name__ == '__main__':
+
+    watch = True
+    deviceList = None
+
+    if sys.argc > 1:
+        if sys.argv[1] == "--watch":
+            watch = True
+
+    Govee = govee_control.Govee()
+
     Govee.log("INFO", "Running smart-control v0.1")
 
     deviceListResponse = Govee.makeRequest("devices")
@@ -10,4 +21,20 @@ if __name__ == '__main__':
         exit(1)
 
     Govee.setDeviceList(deviceListResponse)
-    # print(response)
+
+    try:
+        while watch:
+            device = input("Please select a device to control: ").lower()
+            action = input("Please select the action to take: ").lower()
+
+            deviceList = Govee.getDeviceList()
+
+            for d in deviceList:
+                if device == d['deviceName'].lower():
+                    if action in d['supportCmds']:
+                        Govee.makeRequest()
+
+
+
+    except KeyboardInterrupt:
+        Govee.log("INFO", "Quitting. Goodbye!")
